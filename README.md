e1efb39d2823b957cfce2d05d2d43c7a7c09aeab
nhatvu148 <nhatvu148@gmail.com>
2021-06-04 01:13:43 +0900

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## Enable permissions:
+- deno run --allow-env deno.ts 
+- deno run --allow-all deno.ts 
+- deno run -A deno.ts 
--- deno.ts
+++ deno.ts
@@ -2,5 +2,7 @@ const myVar: string = Deno.args[0];
 
 console.log(`This is my string: ${myVar}`);
 
-// import "https://deno.land/std@0.97.0/examples/welcome.ts";
-import "https://deno.land/std@0.97.0/examples/chat/server.ts";
+console.log("Hello", Deno.env.get("USER"));
+
+import "https://deno.land/std@0.97.0/examples/welcome.ts";
+// import "https://deno.land/std@0.97.0/examples/chat/server.ts";
